42b26cc55cef2dace8d49187ce461bbf9e1025fe
Chris Ortega <chrisortega94@gmail.com>
2023-10-10 02:27:55 -0700

    Complete problem 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...nums) {
+    let finalArr = [];
+    for (let i of arr){
+        //Checks for item from arr in nums rest arg
+        if(!nums.includes(i)){
+            finalArr.push(i);
+        }
+    }
+    return finalArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((num1 < 0) || (num2 < 0) ){
+        return "ERROR";
+    }
+    if ((typeof num1 !== 'number') || (typeof num2 !== 'number')){
+        return "ERROR";
+    }
+    let firstNum = num1;
+    let secondNum = num2;
+    if (num1 > num2){
+        firstNum = num2;
+        secondNum = num1;
+    }
+    let answer = 0;
+    for(let i = firstNum; i <= secondNum; i++){
+        answer += i;
+    }
+    return answer;
 
 };
 
