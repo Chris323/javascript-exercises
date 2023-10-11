0e34f76ece6338dfdfaf2f8dacdba479d0bbf17a
Chris Ortega <chrisortega94@gmail.com>
2023-10-10 17:13:32 -0700

    Complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(yearIn) {
+    if((yearIn%400 !== 0) && (yearIn%100 === 0)){
+        return false;
 
+    }
+    if(yearIn%4 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degInF) {
+  let answer = (degInF - 32)*(5/9);
+  return Math.round(answer * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degInC) {
+  let answer = (degInC * (9/5)) + 32;
+  return Math.round(answer * 10) / 10;
 };
 
 // Do not edit below this line
