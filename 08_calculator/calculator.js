6f8e07bdce9cb85ef5a4de86e81df6b60dc9c841
Chris Ortega <chrisortega94@gmail.com>
2023-12-19 12:31:26 -0800

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  let sum = 0;
+  if (a.length === 0) {
+    return sum;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      sum += a[i];
+    }
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  let product = 1;
+  if (a.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      product *= a[i];
+    }
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let factProd = 1;
+  if(a === 0){
+    return factProd;
+  }
+  else {
+    for (let i = a; i > 0; i--){
+      factProd *= i;
+      console.log('instance', a)
+      console.log('result', factProd);
+    }
+  }
+  return factProd;
+
 };
 
 // Do not edit below this line
@@ -29,5 +57,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
