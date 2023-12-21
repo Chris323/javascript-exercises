22edce9de350283589ab3cafd8945b86fa3d02b0
Chris Ortega <chrisortega94@gmail.com>
2023-12-20 17:16:49 -0800

    Partially finish problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,42 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  if (input.length === 0) {
+    return false;
+  }
+  if (input.length === 1) {
+    return true;
+  }
+  const noSpaceWord = input.split("");
+  let left = 0;
+  let right = noSpaceWord.length - 1;
 
+  let keepGoing = true;
+
+  while (keepGoing) {
+    if (left >= right) {
+      keepGoing = false;
+      break;
+    }
+    if (noSpaceWord[left] === " ") {
+      left++;
+      continue;
+    }
+    if (noSpaceWord[right] === " ") {
+      right--;
+      continue;
+    }
+
+    if (noSpaceWord[left] != noSpaceWord[right]) {
+        console.log(noSpaceWord[left]);
+        console.log(noSpaceWord[right]);
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+  return true;
+
+  //TODO, finish rest of testing units
 };
 
 // Do not edit below this line
