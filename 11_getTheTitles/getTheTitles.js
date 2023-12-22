9dab54e2e6b8c58d27f76e193ce928353aca5ab2
Chris Ortega <chrisortega94@gmail.com>
2023-12-21 16:27:14 -0800

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayIn) {
+    let finalArray = new Array(arrayIn.length);
+    for(let i = 0; i < arrayIn.length; i++){
+        finalArray.splice(i, 1, arrayIn[i].title);
+    }
+    return finalArray;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
