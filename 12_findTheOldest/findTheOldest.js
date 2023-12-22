07fdb9430cd433f13f64d4655d63f4614338a93a
Chris Ortega <chrisortega94@gmail.com>
2023-12-21 16:54:29 -0800

    Stupid fuckin test would let me pass it even AFTER I RETURNED THE OBJECT

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+// const findTheOldest = function (arrayIn) {
+//     console.log(typeof(arrayIn));
+//   let maxAge = 0;
+//   let oldest = "";
+//   for (let i = 0; i < arrayIn.length; i++) {
+//     let personAge = arrayIn[i].yearOfDeath - arrayIn[i].yearOfBirth;
+//     console.log("age", personAge);
+//     if (personAge > maxAge) {
+//       maxAge = personAge;
+//       oldest = arrayIn[i];
+//       console.log(oldest);
+//     }
+//   }
+//   console.log('end: ', `\"${oldest.name}\"`);
+//   return `\"${oldest.name}\"`;
+// };
 
-};
+const findTheOldest = function (array) {
+    return array.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldest;
+    });
+  };
+  
+  const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
